472e8f2c6d2864c7a5bcc91aa0bdd84b1cb15576
Cadu Romaniello <cadu_romaniello@hotmail.com>
2025-04-27 16:40:44 -0300

    add: logs

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ PLAY_RELEVANT_COLUMNS = ['gameId', 'playId', 'quarter', 'down', 'yardsToGo', 'po
 TRACKING_RELEVANTCOLUMNS = ['nflId', 'club', 'playDirection', 'x', 'y', 's', 'a', 'dis', 'o', 'dir', 'height', 'weight', 'position', 'totalDis']
 N_CLOSEST_PLAYERS = 2
 RANDOM_SEED = 1
-NUMBER_OF_ITERS = 5
+NUMBER_OF_ITERS = 6
 
 CONFIG = {
     'RANDOM_SEED': 1,
@@ -53,7 +53,7 @@ def main():
     pass_graphs = []
     random_seed = 0
     random.seed(random_seed)
-    random_seed = 13
+    random_seed = 18
     
     for week in weeks:
         print('--------------------------------------------------------')
